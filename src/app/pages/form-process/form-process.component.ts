ccac41fe13e2cb9164e70b08dc359ed5a49ea380
valdas.gliaubertas <valdas.gliaubertas@ferrattum.com>
2017-12-28 17:11:16 +0200

    angular

--- src/app/pages/form-process/form-process.component.ts
+++ src/app/pages/form-process/form-process.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-form-process',
@@ -17,9 +18,9 @@ export class FormProcessComponent implements OnInit {
 
   ngOnInit() {
       this.lead = new FormGroup({
-          name: new FormControl(''),
-          email: new FormControl(''),
-          phone: new FormControl(''),
+          name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+          email: new FormControl('', [Validators.required, Validators.email]),
+          phone: new FormControl('', [Validators.minLength(12), Validators.pattern("\\+3706[0-9]{7}")]),
           consent: new FormControl('')
       });
   }
